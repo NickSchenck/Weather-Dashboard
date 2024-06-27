dcf82ab8ad882639f4e561b67592d3b026036389
unknown <yoyo4games@Yahoo.com>
2024-06-27 15:39:36 -0500

    Changed how the API call is functioning. I think I will split-up those two calls, into more specialized functions. Most time was spent reading and familiarizing myself with the openweathermap documentation, and figuring out which products of theirs were free-to-use. Left very basic notes on potential development.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,94 @@
 dayjs.extend(window.dayjs_plugin_utc);      //First two lines enable dayjs to iterate through dates in the five day forecast
 dayjs.extend(window.dayjs_plugin_timezone);
 let searchSection = document.querySelector("#search-section");
-let searchCity = document.querySelector("#search-area");
+
 let time = moment(new Date()).format("MM/DD/YYYY");     //This line uses moment.js to determine the current days date in the inital city display
 let historyClicked = document.querySelector("#search-history");
-let baseURL = "http://api.openweathermap.org/data/2.5/weather?";
+let baseURL = "http://api.openweathermap.org/data/2.5/";
 let apiKey = "88492f617957986cb392da3e78550452";
+let searchCity = document.querySelector("#search-area");
 let city = searchCity.value.trim();
 //complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
-let formSubmitHandler = function (event) {
+function formSubmitHandler(event) {
   event.preventDefault();
        //Lines 9-13 prevent the page from reloading, get the value of the search box, and prevent the user from entering an empty response
   if (city === "") {
-    alert("You must enter a valid city")
+    alert("You must enter a valid city.")
   }
   let citySave = document.createElement("button");
   citySave.classList.add("d-flex", "flex-column", "align-items-stretch", "border-0", "bg-secondary", "w-100", "text-center", "mt-2", "rounded");    //lines 14-17 display the searched city as a button
   citySave.innerText = city;
   document.getElementById("search-history").appendChild(citySave);
   document.getElementById("city-searched").innerText = city + " " + time;
-  fetch(`${baseURL}appid=${apiKey}&q=${city}`)   //lines 18-26 display the city and date in inital results box and makes an API call using the cities name as a parameter
+  getWeather();
+};
+
+/*Both API calls here seem to be working as structured, but I think I'll want to break them up into different functions. One idea that
+immediatly seems better is to have two functions called by getWeather. The first function, displayWeather is already being called. The
+second function, maybe something like determineFullForecast, would also recieve data while being called from getWeather, and would be
+intended to select specific points of data from the large response we get, then package that into an object which we could pass to a
+function that'd display the 5-day forecast.*/
+function getWeather(){
+  fetch(`${baseURL}weather?appid=${apiKey}&q=${city}&units=imperial`)   //lines 18-26 display the city and date in inital results box and makes an API call using the cities name as a parameter
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      getLatLon(data)
-      console.log(data);
+      let lat = data.coord.lat
+      let lon = data.coord.lon
+      fetch(`${baseURL}forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(data){
+          console.log(data)
+          displayWeather(data);
+        });
     });
 };
+//.then(function (data) {
+//   displayWeather(data)
+// });
 
 /*We're going to need to look into tutorials on how to interact with the openweather API website. I think they may have changed how things
 work on their end, and we will need to update accordingly.*/
-let getLatLon = function (data) {
-  let tz =  data.timezon;
-  let lat = data.coord.lat;       //This function gets us our Timezone, and coordinates
-  let lon = data.coord.lon;
-  getWeather(lat, lon, tz);
-};
+// let getLatLon = function (data) {
+//   let tz =  data.timezone;
+//   let lat = data.coord.lat;       //This function gets us our Timezone, and coordinates
+//   let lon = data.coord.lon;
+//   getWeather(lat, lon, tz);
+// };
 
-let getWeather = function (lat, lon, tz) {
-  fetch(`${baseURL}appid=${apiKey}&q=${city}`)  //this function uses coordinates as parameters for an API call to get weather data
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      displayWeather(data, tz)
-      // console.log(dayjs().tz(tz).add(1, "day").startOf("day").format("M/D/YYYY"))
-    });
-}
+// let getWeather = function (lat, lon, tz) {
+//   fetch(`${baseURL}appid=${apiKey}&q=${city}`)  //this function uses coordinates as parameters for an API call to get weather data
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       displayWeather(data, tz)
+//       // console.log(dayjs().tz(tz).add(1, "day").startOf("day").format("M/D/YYYY"))
+//     });
+// };
 
-let displayWeather = function (weather, tz) {
-  console.log(weather);
-  document.getElementById("city-temp").innerText = "Temp: " + weather.current.temp + "°F";
-  document.getElementById("city-wind").innerText = "Wind: " + weather.current.wind_speed + "MPH";     //lines 48-51 display weather data in the inital results box
-  document.getElementById("city-humid").innerText = "Humidity: " + weather.current.humidity + "%";
+// fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=88492f617957986cb392da3e78550452&units=imperial")  //this function uses coordinates as parameters for an API call to get weather data
+// .then(function (response) {
+//   return response.json();
+// })
+// .then(function (data) {
+//   displayWeather(data, tz)
+//   // console.log(dayjs().tz(tz).add(1, "day").startOf("day").format("M/D/YYYY"))
+// });
+// }
+
+function displayWeather(weather) {
+  // console.log(weather)
+  document.getElementById("city-temp").innerText = "Temp: " + weather.main.temp + "°F";
+  document.getElementById("city-wind").innerText = "Wind: " + weather.wind.speed + "MPH";     //lines 48-51 display weather data in the inital results box
+  document.getElementById("city-humid").innerText = "Humidity: " + weather.main.humidity + "%";
   document.getElementById("city-uv").innerText = "UV Index: " + weather.current.uvi;
-  for(i = 0; i < 5; i++){
-    let date = dayjs().tz(tz).add(i, "day").startOf("day").format("M/D/YYYY");
+  for(i = 0; i < 5; i++){ //this for loop was determining where specific information of the 5-day forecast was being displayed
+    // let date = dayjs().tz(tz).add(i, "day").startOf("day").format("M/D/YYYY");
   document.getElementById("date" + (i + 1)).innerHTML = date;
   icon = weather.daily[(i + 1)].weather[0].icon;
   let iconEl = document.createElement("img");
